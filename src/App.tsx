7e3df0834c3b05c546e716c2cae8fe6d0c296142
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-02-22 17:36:44 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Bienvenido/a a beerdin!</Button>
+    </>
+  );
+}
+
+export default App;
