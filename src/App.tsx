6b7e0443be9bad19d87b1b0b6a46d09e9efeb16a
Jarrison Cano <me@jarrison.dev>
2025-02-28 12:02:01 -0500

    fix: remove test console log from App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Navbar } from '@/components/Navbar/Navbar';
 import { Button } from './components/ui/button';
 
 function App() {
-  console.log('test');
   return (
     <>
       <Navbar />
