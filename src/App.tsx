44a444f0f88b2966a6ab7d2032330ddea8ccbef1
Jarrison Cano <me@jarrison.dev>
2025-02-28 11:56:50 -0500

    chore: add console log for testing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Navbar } from '@/components/Navbar/Navbar';
 import { Button } from './components/ui/button';
 
 function App() {
+  console.log('test');
   return (
     <>
       <Navbar />
